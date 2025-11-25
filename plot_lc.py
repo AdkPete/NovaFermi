36d1c05c04f07b7a0ed81f6ba3f601a23f7fb014
Peter Craig <pac4607@rit.edu>
2025-11-25 11:18:03 -0500

    Added routine to plot light curve data

---
+++ plot_lc.py
@@ -0,0 +1,56 @@
+'''
+Short routine to collect and plot the results of a light curve
+generated with analyze_fermi.py
+'''
+
+import matplotlib.pyplot as plt
+import sys , os
+import analyze_fermi as af
+import numpy as np
+
+params = af.read_parameters(sys.argv[1])
+
+dir = "./"
+Flux = []
+Unc = []
+Time = []
+TS = []
+
+for i in os.listdir(dir):
+    fname = os.path.join(dir , i)
+    if ".csv" not in fname or "mp" not in fname:
+        continue
+    f = open(fname)
+    for line in f.readlines():
+        split_line = line.split(",")
+        Flux.append(float(split_line[0]))
+        Unc.append(float(split_line[1]))
+        TS.append(float(split_line[2]))
+        MET = float(split_line[3])
+        tpeak = af.met_to_tpeak(MET , params)
+        Time.append(tpeak)
+        break
+    
+Time = np.array(Time)
+TS = np.array(TS)
+Unc = np.array(Unc)
+Flux = np.array(Flux)
+
+
+plt.scatter(Time , TS)
+plt.xlabel("Time since Peak (Days)")
+plt.ylabel("TS")
+plt.savefig("TSFig.pdf")
+plt.show()
+
+det = np.where(TS >= 4)
+lim = np.where(TS < 4)
+
+plt.scatter(Time[det] , Flux[det], color = "blue")
+plt.errorbar(Time[det] , Flux[det] , Unc[det] , ls = 'none', color = "blue")
+#plt.scatter(Time[lim] , Flux[lim] , color = "orange" , marker = "v")
+plt.yscale('log')
+plt.xlabel("Time since peak (days)")
+plt.ylabel("Flux (ph / s / cm$^{-2}$)")
+plt.show()
+        
