805a44d46ade8bf43da109bfc7a4d6948fa381db
Peter Craig <pac4607@rit.edu>
2025-11-25 15:08:38 -0500

    Added functions to combine data into a single csv file

--- plot_lc.py
+++ plot_lc.py
@@ -9,7 +9,7 @@ import analyze_fermi as af
 import numpy as np
 
 
-def plot_light_curve(params, display=False):
+def plot_light_curve(params, display=False, compile_csv = None):
     '''
     Function to plot light curve results from analyze_fermi
     Assumes that you are running in the directory with the data
@@ -25,30 +25,28 @@ def plot_light_curve(params, display=False):
     __________
     params : dict : parameter dict from read_parameters
     display : boolean : If true, run plt.show() to display figures
+    compile_csv : string : Name of input csv file
     Returns
     _______
     None
     '''
-    dir = "./"
-    Flux = []
-    Unc = []
+    if compile_csv is None:
+        compiled_csv = params["name"] + f"_{int(params['window'])}_lcdata.csv"
+        
     Time = []
     TS = []
-
-    for i in os.listdir(dir):
-        fname = os.path.join(dir , i)
-        if ".csv" not in fname or "mp" not in fname:
+    Flux = []
+    Unc = []
+    
+    f = open(compiled_csv)
+    for i in f.readlines():
+        if "TS" in i:
             continue
-        f = open(fname)
-        for line in f.readlines():
-            split_line = line.split(",")
-            Flux.append(float(split_line[0]))
-            Unc.append(float(split_line[1]))
-            TS.append(float(split_line[2]))
-            MET = float(split_line[3])
-            tpeak = af.met_to_tpeak(MET , params)
-            Time.append(tpeak)
-            break
+        sl = i.split(",")
+        Time.append(float(sl[0]))
+        TS.append(float(sl[2]))
+        Flux.append(float(sl[3]))
+        Unc.append(float(sl[4]))
         
     Time = np.array(Time)
     TS = np.array(TS)
@@ -73,12 +71,73 @@ def plot_light_curve(params, display=False):
     plt.yscale('log')
     plt.xlabel("Time since peak (days)")
     plt.ylabel("Flux (ph / s / cm$^{-2}$)")
-    if display():
+    if display:
         
         plt.show()
     else:
         plt.close()
 
+def compile_data(params, output=None):
+    '''
+    Utility function to compile all of the multi-processing logs into 
+    a singular csv file.
+    
+    Parameters
+    __________
+    
+    params : dict : parameter dict from read_parameters
+    output : string : name of output csv file
+    
+    Returns
+    _______
+    None
+    
+    '''
+    if output is None:
+        output = params["name"] + f"_{int(params['window'])}_lcdata.csv"
+    
+    
+    dir = "./"
+    Flux = []
+    Unc = []
+    Time = []
+    TS = []
+    METs = []
+    for i in os.listdir(dir):
+        fname = os.path.join(dir , i)
+        if ".csv" not in fname or "mp" not in fname:
+            continue
+        f = open(fname)
+        for line in f.readlines():
+            split_line = line.split(",")
+            Flux.append(float(split_line[0]))
+            Unc.append(float(split_line[1]))
+            TS.append(float(split_line[2]))
+            MET = float(split_line[3])
+            METs.append(MET)
+            tpeak = af.met_to_tpeak(MET , params)
+            Time.append(tpeak)
+            break
+        
+    METs = np.array(METs)
+    TS = np.array(TS)
+    Flux = np.array(Flux)
+    Unc = np.array(Unc)
+    Time = np.array(Time)
+    
+    isort = np.argsort(Time)
+    out_file = open(output, "w")
+    
+    header = "Time since peak (days),Fermi MET (seconds),TS,Flux,Flux"
+    header += " Uncertainty\n"
+    out_file.write(header)
+    
+    for ind in isort:
+        csv_line = f"{Time[ind]},{METs[ind]},{TS[ind]},{Flux[ind]},{Unc[ind]}"
+        out_file.write(csv_line + "\n")
+    out_file.close()
+    
 if __name__ == "__main__":
     params = af.read_parameters(sys.argv[1])
+    compile_data(params)
     plot_light_curve(params)
