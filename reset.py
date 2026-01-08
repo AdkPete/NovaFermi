35daec57916a707ec7ca70c2134d63f311d49222
Peter Craig <pac4607@rit.edu>
2026-01-08 11:02:31 -0500

    Updated reset.py to include warnings and checks to prevent accidentally deleting anything

--- reset.py
+++ reset.py
@@ -1,5 +1,6 @@
 import os
 
+files = []
 for i in os.listdir("./"):
     if "PH" in i and ".fits" in i:
         continue
@@ -13,4 +14,32 @@ for i in os.listdir("./"):
         continue 
     if "." not in i:
         continue
+    files.append(i)
+    
+print (f"Warning: This script is about to delete {len(files)} files!!")
+ftypes = {}
+for fn in files:
+    ft = fn.split(".")[-1]
+    if ft in ftypes.keys():
+        ftypes[ft] += 1
+    else:
+        ftypes[ft] = 1
+        
+print ("These files include:")
+
+for ft in ftypes.keys():
+    print (f"{ftypes[ft]} {ft} Files")
+
+check1 = input("Do you want to proceed? (Y/N) --> ")
+
+if check1.lower() != "y":
+    print ("Cancelling")
+    exit()
+
+check2 = input(f"Final Check: Delete {len(files)} files? (Y/N) --> ")
+
+if check2.lower() != "y":
+    print ("Cancelling")
+    exit()
+for i in files:
     os.remove(i)
